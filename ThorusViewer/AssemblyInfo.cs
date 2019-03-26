2b216b363aed840576227b12b445d398b25f4203
Octavian Paraschiv <octavian.paraschiv@cegeka.com>
2019-03-26 13:53:56 +0200

    added assembly info

---
+++ ThorusViewer/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using ThorusCommon.IO;
+
+[assembly: AssemblyTitle("ThorusViewer")]
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyCompany(Constants.Company)]
+[assembly: AssemblyProduct(Constants.Product)]
+[assembly: AssemblyCopyright(Constants.Copyright)]
+[assembly: AssemblyVersion(VersionInfo.Version)]
+[assembly: AssemblyFileVersion(VersionInfo.Version)]
